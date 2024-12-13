7b2a74fade8ef53c4c1173b8ea58294eb4466ebd
hosu <76641246+junghojune@users.noreply.github.com>
2024-12-13 14:40:27 +0900

    first commit

---
+++ priceradar/src/main/java/com/cha/priceradar/PriceradarApplication.java
@@ -0,0 +1,13 @@
+package com.cha.priceradar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceradarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceradarApplication.class, args);
+	}
+
+}
---
+++ priceradar/src/test/java/com/cha/priceradar/PriceradarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cha.priceradar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceradarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
