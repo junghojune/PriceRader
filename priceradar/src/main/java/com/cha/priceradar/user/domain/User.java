14f609207e4bfaf05270b32385a024e2bfe196d4
hosu <76641246+junghojune@users.noreply.github.com>
2024-12-17 17:40:37 +0900

    add : 데이터 모델링 user
    user 도메인 구성

---
+++ priceradar/src/main/java/com/cha/priceradar/user/domain/User.java
@@ -0,0 +1,32 @@
+package com.cha.priceradar.user.domain;
+
+import com.cha.priceradar.common.domain.UserRole;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "pr_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private UserRole role;
+}
+
