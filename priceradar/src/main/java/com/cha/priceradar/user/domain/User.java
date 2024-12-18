1d9bb90e3d6fd1331ff3e1521fbfb1d16e25283a
hosu <76641246+junghojune@users.noreply.github.com>
2024-12-18 17:10:46 +0900

    update : 데이터 생성 가능하도록 추가
    테스트에 사용할 수 있도록 기능 추가

--- priceradar/src/main/java/com/cha/priceradar/user/domain/User.java
+++ priceradar/src/main/java/com/cha/priceradar/user/domain/User.java
@@ -10,7 +10,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "pr_user")
 public class User extends BaseEntity {
