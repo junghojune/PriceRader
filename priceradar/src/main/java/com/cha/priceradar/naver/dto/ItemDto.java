4980637e8042a2ae4b78383ebeb8930907c0d507
hosu <76641246+junghojune@users.noreply.github.com>
2025-01-06 12:16:48 +0900

    update : naver api 개발
    네이버 쇼핑에서 특정 검색 물품 가져오도록 개발 완료

---
+++ priceradar/src/main/java/com/cha/priceradar/naver/dto/ItemDto.java
@@ -0,0 +1,24 @@
+package com.cha.priceradar.naver.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.json.JSONObject;
+
+@Getter
+@NoArgsConstructor
+public class ItemDto {
+    private String title;
+    private String link;
+    private String image;
+    private int lprice;
+    private String brand;
+
+
+    public ItemDto(JSONObject itemJson) {
+        this.title = itemJson.getString("title");
+        this.link = itemJson.getString("link");
+        this.image = itemJson.getString("image");
+        this.lprice = itemJson.getInt("lprice");
+        this.brand = itemJson.getString("brand");
+    }
+}
