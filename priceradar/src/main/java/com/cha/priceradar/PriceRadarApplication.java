67e8785de2f220d6c46ebaa2665cc75c31b7885f
hosu <76641246+junghojune@users.noreply.github.com>
2025-01-07 18:38:12 +0900

    update : 스케줄러 어노테이션 추가
    스케줄러 어노테이션 추가

--- priceradar/src/main/java/com/cha/priceradar/PriceRadarApplication.java
+++ priceradar/src/main/java/com/cha/priceradar/PriceRadarApplication.java
@@ -2,9 +2,11 @@ package com.cha.priceradar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
+@EnableScheduling
 public class PriceRadarApplication {
 
 	public static void main(String[] args) {
